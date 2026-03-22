6077de704a775fb9539566ecd41366e56424aabf
DEEPAN <141644376+deepanmpc@users.noreply.github.com>
2026-03-22 23:28:20 +0530

    feat:crawler can store more meta data

--- src/crawler.py
+++ src/crawler.py
@@ -10,10 +10,18 @@ def crawl_directory(folder_path):
             ext = path.suffix.lower()
             
             if ext in SUPPORTED_EXTENSIONS:
+                stat = path.stat()
+                
                 files.append({
+                    "name": path.name,
                     "path": str(path),
+                    "abs_path": str(path.resolve()),
                     "type": ext,
-                    "modified": path.stat().st_mtime
+                    "size": stat.st_size,
+                    "modified": stat.st_mtime,
+                    "created": stat.st_ctime,
+                    "accessed": stat.st_atime,
+                    "parent": str(path.parent)
                 })
     
     return files
